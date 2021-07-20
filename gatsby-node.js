fe0198d860c24e798594ba3cc89c7c5af7ff160e
Morgane Dubus <morgane.d@meilisearch.com>
2021-07-20 15:37:09 +0200

    Validate plugin options

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,23 @@
-exports.pluginOptionsSchema = ({ Joi }) => {
-  return Joi.object({
-    host: Joi.string().required(),
-    apiKey: Joi.any(),
-    skipIndexing: Joi.any(),
-    indexes: Joi.object({
-      indexUid: Joi.any(),
-      query: Joi.string().required(),
-    }),
-  })
+const validatePluginOptions = config => {
+  if (!config.host) {
+    throw new Error(`gatsby-plugin-meilisearch: option "host" missing`)
+  }
+  if (!config.indexes || !config.indexes.query) {
+    throw new Error(`gatsby-plugin-meilisearch: option "indexes.query" missing`)
+  }
 }
 
 exports.onPostBuild = async function ({ graphql, reporter }, config) {
-  const activity = reporter.activityTimer(`Running gatsby-plugin-meilisearch`)
+  const activity = reporter.activityTimer(`gatsby-plugin-meilisearch`)
   activity.start()
   try {
+    validatePluginOptions(config)
     const { indexes } = config
     const { data } = await graphql(indexes.query)
     console.log(data)
   } catch (err) {
     reporter.error(err)
+    activity.setStatus('Failed to index to MeiliSearch')
   }
   activity.end()
 }
