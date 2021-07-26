ca8a5212fba41300fcf01431d88346133dae8fe9
Morgane Dubus <morgane.d@meilisearch.com>
2021-07-26 11:29:16 +0200

    Update tests

--- cypress/integration/home_page.specs.js
+++ cypress/integration/home_page.specs.js
@@ -9,21 +9,6 @@ describe(`Home page`, () => {
   it('Should have a title', () => {
     cy.contains('MeiliSearch + React InstantSearch + Gatsby')
   })
-  it('Should have an article list of 5 elements', () => {
-    cy.contains('Article List')
-    cy.get('.article-list li').should('have.length', 5)
-    cy.get('.article-list li')
-      .first()
-      .should('have.text', 'Axolotl')
-      .next()
-      .should('have.text', 'Immortal Jellyfish')
-      .next()
-      .should('have.text', 'Marmoset')
-      .next()
-      .should('have.text', 'Okapi')
-      .next()
-      .should('have.text', 'Shoebill')
-  })
   it('Should have a search bar', () => {
     cy.contains('Search in this blog’s articles')
     cy.get('input[type="search"]').should('be.visible')
